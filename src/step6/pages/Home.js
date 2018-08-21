bd7fbfa2bb8cee55c6adb3d2bc078199b5be4694
ri382j <ri382j@intl.att.com>
2018-08-21 12:35:07 +0300

    [step6] create Home page

--- src/step6/pages/Home.js
+++ src/step6/pages/Home.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
 import User from '../users/components/User'
 import Pagination from '../users/components/Pagination';
-import EditUser from '../users/components/EditUser';
 import UsersApi from '../api/users'
+import { Link } from 'react-router-dom';
 export default class Home extends Component {
     constructor() {
         super();
@@ -18,34 +18,6 @@ export default class Home extends Component {
             users
         })
     }
-    selectUser = (id) => {
-        this.setState((oldState) => {
-            const editUser = { ...oldState.users.find((u) => id === u.id) };
-            return {
-                editUser
-            }
-        })
-    }
-    saveEditUser = async () => {
-        let editUser = await UsersApi.updateUser(this.state.editUser);
-        this.setState((oldState) => ({
-            editUser,
-            users: oldState.users.map((u) => u.id !== editUser.id ? u : editUser)
-        }))
-    }
-    editUserChange = (e) => {
-        let editUser = {
-            [e.target.name]: e.target.value
-        }
-        this.setState((oldState) => {
-            return {
-                editUser: {
-                    ...oldState.editUser,
-                    ...editUser,
-                }
-            }
-        })
-    }
     nextPage = (e) => {
         this.setState({
             startIndex: e.startIndex,
@@ -53,15 +25,9 @@ export default class Home extends Component {
         })
     }
     render() {
-        let editUser = ''
-        if (this.state.editUser) {
-            editUser = <EditUser {...this.state.editUser}
-                save={this.saveEditUser}
-                onChange={this.editUserChange} />
-        }
         return (
-            <div className="row">
-                <div className="col-xs-12 col-sm-8 list-group">
+            
+                <div className="col-sm-12 list-group">
                     <ul className="list-group">
                         <Pagination inPage={this.state.inPage} startIndex={this.state.startIndex} nextPage={this.nextPage}>
                             {this.state.users
@@ -71,19 +37,17 @@ export default class Home extends Component {
                                 })
                                 .map((u) => (
                                     <li key={u.id}
-                                        onClick={this.selectUser.bind(this, u.id)}
                                         className={"list-group-item " + u.active}
                                     >
-                                        <User {...u} />
+                                        <Link to={"/user/" + u.id}>
+                                            <User {...u} />
+                                        </Link>
+
                                     </li>
                                 ))}
                         </Pagination>
                     </ul>
                 </div>
-                <div className={'col-xs-12 col-sm-4'}>
-                    {editUser}
-                </div>
-            </div>
         )
     }
 }
